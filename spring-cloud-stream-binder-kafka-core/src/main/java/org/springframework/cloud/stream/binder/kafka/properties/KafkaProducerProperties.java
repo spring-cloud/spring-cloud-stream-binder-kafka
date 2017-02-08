7a822cec140076cc76c3457d37ff8852641fd48b
Ilayaperumal Gopinathan <igopinathan@pivotal.io>
2017-02-08 16:38:33 +0530

    Support Spring Boot KafkaProperties
     - If KafkaProperties for the KafkaAutoConfiguration is set, then use those properties for the KafkaMessageChannelBinder
     - For the KafkaProperties that have explicit default, override with the KafkaMessageChannelBinder defaults when the properties are not set by any of the property sources
     - Support the existing Kafka Producer/Consumer properties if they are set
     - Add tests
    
    Resolves #73

--- spring-cloud-stream-binder-kafka-core/src/main/java/org/springframework/cloud/stream/binder/kafka/properties/KafkaProducerProperties.java
+++ spring-cloud-stream-binder-kafka-core/src/main/java/org/springframework/cloud/stream/binder/kafka/properties/KafkaProducerProperties.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 the original author or authors.
+ * Copyright 2016-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,8 +26,10 @@ import javax.validation.constraints.NotNull;
  */
 public class KafkaProducerProperties {
 
+	@Deprecated
 	private int bufferSize = 16384;
 
+	@Deprecated
 	private CompressionType compressionType = CompressionType.none;
 
 	private boolean sync;
@@ -80,6 +82,7 @@ public class KafkaProducerProperties {
 	public enum CompressionType {
 		none,
 		gzip,
-		snappy
+		snappy,
+		lz4
 	}
 }
