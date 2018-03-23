ece179a5932b89eef94334c8bc35445a96e88cbd
Lei Chen <lchen576@bloomberg.net>
2018-03-23 12:52:12 -0700

    Allow state store creation when using process/transform method in DSL

---
+++ spring-cloud-stream-binder-kafka-streams/src/main/java/org/springframework/cloud/stream/binder/kafka/streams/annotations/KafkaStreamsStateStore.java
@@ -0,0 +1,4 @@
+package org.springframework.cloud.stream.binder.kafka.streams.annotations;
+
+public interface KafkaStreamsStateStore {
+}
---
+++ spring-cloud-stream-binder-kafka-streams/src/main/java/org/springframework/cloud/stream/binder/kafka/streams/properties/KafkaStreamsStateStoreProperties.java
@@ -0,0 +1,4 @@
+package org.springframework.cloud.stream.binder.kafka.streams.properties;
+
+public class KafkaStreamsStateStoreProperties {
+}
