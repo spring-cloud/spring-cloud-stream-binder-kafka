40b53ee58bcfd0d2cca17f1419f30ffd2a31a5bb
Soby Chacko <schacko@pivotal.io>
2019-04-08 14:18:43 -0400

    Transactional binder producer factory
    With a transactional binder, the producer factory should not be destroyed.
    
    Resolves #626

--- spring-cloud-stream-binder-kafka/src/main/java/org/springframework/cloud/stream/binder/kafka/KafkaMessageChannelBinder.java
+++ spring-cloud-stream-binder-kafka/src/main/java/org/springframework/cloud/stream/binder/kafka/KafkaMessageChannelBinder.java
@@ -214,7 +214,9 @@ public class KafkaMessageChannelBinder extends
 					Producer<byte[], byte[]> producer = producerFB.createProducer();
 					List<PartitionInfo> partitionsFor = producer.partitionsFor(destination.getName());
 					producer.close();
-					((DisposableBean) producerFB).destroy();
+					if (this.transactionManager == null) {
+						((DisposableBean) producerFB).destroy();
+					}
 					return partitionsFor;
 				});
 		this.topicsInUse.put(destination.getName(), new TopicInformation(null, partitions));
