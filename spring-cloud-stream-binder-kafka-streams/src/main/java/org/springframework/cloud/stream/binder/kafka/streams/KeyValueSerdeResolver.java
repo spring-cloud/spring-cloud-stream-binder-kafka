fc1e4602fb021d4afda0dde9971f2f2ab2e028b8
Adriano Scheffer <adriano.scheffer@traderev.com>
2020-02-06 18:41:23 -0500

    Always call value serde configure method
    Resolves https://github.com/spring-cloud/spring-cloud-stream-binder-kafka/issues/836
    
    Remove redundant call to serde configure
    
    Closes https://github.com/spring-cloud/spring-cloud-stream-binder-kafka/issues/836

--- spring-cloud-stream-binder-kafka-streams/src/main/java/org/springframework/cloud/stream/binder/kafka/streams/KeyValueSerdeResolver.java
+++ spring-cloud-stream-binder-kafka-streams/src/main/java/org/springframework/cloud/stream/binder/kafka/streams/KeyValueSerdeResolver.java
@@ -200,7 +200,7 @@ public class KeyValueSerdeResolver {
 									Serde.class)
 							: Serdes.ByteArray();
 		}
+		valueSerde.configure(this.streamConfigGlobalProperties, false);
 		return valueSerde;
 	}
-
 }
