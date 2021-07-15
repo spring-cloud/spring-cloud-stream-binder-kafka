2495e6e44e3b8c6c0455447583d474445e9ac100
Soby Chacko <schacko@pivotal.io>
2021-07-15 12:54:49 -0400

    Revert "JUnit 5 migration related to boot 2.6"
    This reverts commit 1a45ff797a1fa4ed5eaf32337cbee650c0bb3d29.

--- spring-cloud-stream-binder-kafka/src/test/java/org/springframework/cloud/stream/binder/kafka/KafkaBinderTests.java
+++ spring-cloud-stream-binder-kafka/src/test/java/org/springframework/cloud/stream/binder/kafka/KafkaBinderTests.java
@@ -66,11 +66,12 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.assertj.core.api.Assertions;
 import org.assertj.core.api.Condition;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInfo;
-import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import org.springframework.beans.DirectFieldAccessor;
 import org.springframework.cloud.stream.binder.Binder;
@@ -97,6 +98,7 @@ import org.springframework.cloud.stream.binder.kafka.utils.DlqPartitionFunction;
 import org.springframework.cloud.stream.binder.kafka.utils.KafkaTopicUtils;
 import org.springframework.cloud.stream.binding.MessageConverterConfigurer.PartitioningInterceptor;
 import org.springframework.cloud.stream.config.BindingProperties;
+import org.springframework.cloud.stream.provisioning.ProvisioningException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
@@ -163,14 +165,14 @@ public class KafkaBinderTests extends
 	// @checkstyle:on
 	private static final int DEFAULT_OPERATION_TIMEOUT = 30;
 
-//	@RegisterExtension
-//	public ExpectedException expectedProvisioningException = ExpectedException.none();
+	@Rule
+	public ExpectedException expectedProvisioningException = ExpectedException.none();
 
 	private final String CLASS_UNDER_TEST_NAME = KafkaMessageChannelBinder.class
 			.getSimpleName();
 
-	@RegisterExtension
-	public static EmbeddedKafkaRule embeddedKafka = new EmbeddedKafkaRuleExtension(1, true, 10,
+	@ClassRule
+	public static EmbeddedKafkaRule embeddedKafka = new EmbeddedKafkaRule(1, true, 10,
 			"error.pollableDlq.group-pcWithDlq")
 					.brokerProperty("transaction.state.log.replication.factor", "1")
 					.brokerProperty("transaction.state.log.min.isr", "1");
@@ -189,12 +191,8 @@ public class KafkaBinderTests extends
 		return kafkaConsumerProperties;
 	}
 
-	private ExtendedProducerProperties<KafkaProducerProperties> createProducerProperties() {
-		return this.createProducerProperties(null);
-	}
-
 	@Override
-	protected ExtendedProducerProperties<KafkaProducerProperties> createProducerProperties(TestInfo testInto) {
+	protected ExtendedProducerProperties<KafkaProducerProperties> createProducerProperties() {
 		ExtendedProducerProperties<KafkaProducerProperties> producerProperties = new ExtendedProducerProperties<>(
 				new KafkaProducerProperties());
 		producerProperties.getExtension().setSync(true);
@@ -276,7 +274,7 @@ public class KafkaBinderTests extends
 		return KafkaHeaders.OFFSET;
 	}
 
-	@BeforeEach
+	@Before
 	public void init() {
 		String multiplier = System.getenv("KAFKA_TIMEOUT_MULTIPLIER");
 		if (multiplier != null) {
@@ -554,7 +552,7 @@ public class KafkaBinderTests extends
 	@Test
 	@Override
 	@SuppressWarnings("unchecked")
-	public void testSendAndReceiveNoOriginalContentType(TestInfo testInfo) throws Exception {
+	public void testSendAndReceiveNoOriginalContentType() throws Exception {
 		Binder binder = getBinder();
 
 		BindingProperties producerBindingProperties = createProducerBindingProperties(
@@ -604,7 +602,7 @@ public class KafkaBinderTests extends
 	@Test
 	@Override
 	@SuppressWarnings("unchecked")
-	public void testSendAndReceive(TestInfo testInfo) throws Exception {
+	public void testSendAndReceive() throws Exception {
 		Binder binder = getBinder();
 		BindingProperties outputBindingProperties = createProducerBindingProperties(
 				createProducerProperties());
@@ -730,7 +728,7 @@ public class KafkaBinderTests extends
 
 	@Test
 	@SuppressWarnings("unchecked")
-	@Disabled
+	@Ignore
 	public void testDlqWithNativeSerializationEnabledOnDlqProducer() throws Exception {
 		Binder binder = getBinder();
 		ExtendedProducerProperties<KafkaProducerProperties> producerProperties = createProducerProperties();
@@ -1461,15 +1459,9 @@ public class KafkaBinderTests extends
 		producerBinding.unbind();
 	}
 
-	@Test
+	@Test(expected = IllegalArgumentException.class)
 	public void testValidateKafkaTopicName() {
-		try {
-			KafkaTopicUtils.validateTopicName("foo:bar");
-			fail("Expecting IllegalArgumentException");
-		}
-		catch (Exception e) {
-			// TODO: handle exception
-		}
+		KafkaTopicUtils.validateTopicName("foo:bar");
 	}
 
 	@Test
@@ -1607,7 +1599,7 @@ public class KafkaBinderTests extends
 	@Test
 	@Override
 	@SuppressWarnings("unchecked")
-	public void testSendAndReceiveMultipleTopics(TestInfo testInfo) throws Exception {
+	public void testSendAndReceiveMultipleTopics() throws Exception {
 		Binder binder = getBinder();
 
 		DirectChannel moduleOutputChannel1 = createBindableChannel("output1",
@@ -1768,7 +1760,7 @@ public class KafkaBinderTests extends
 	@Test
 	@Override
 	@SuppressWarnings("unchecked")
-	public void testTwoRequiredGroups(TestInfo testInfo) throws Exception {
+	public void testTwoRequiredGroups() throws Exception {
 		Binder binder = getBinder();
 		ExtendedProducerProperties<KafkaProducerProperties> producerProperties = createProducerProperties();
 
@@ -1818,7 +1810,7 @@ public class KafkaBinderTests extends
 	@Test
 	@Override
 	@SuppressWarnings("unchecked")
-	public void testPartitionedModuleSpEL(TestInfo testInfo) throws Exception {
+	public void testPartitionedModuleSpEL() throws Exception {
 		Binder binder = getBinder();
 
 		ExtendedConsumerProperties<KafkaConsumerProperties> consumerProperties = createConsumerProperties();
@@ -1941,7 +1933,7 @@ public class KafkaBinderTests extends
 	}
 
 	@Test
-//	@Override
+	@Override
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public void testPartitionedModuleJava() throws Exception {
 		Binder binder = getBinder();
@@ -2029,7 +2021,7 @@ public class KafkaBinderTests extends
 	@Test
 	@Override
 	@SuppressWarnings("unchecked")
-	public void testAnonymousGroup(TestInfo testInfo) throws Exception {
+	public void testAnonymousGroup() throws Exception {
 		Binder binder = getBinder();
 		BindingProperties producerBindingProperties = createProducerBindingProperties(
 				createProducerProperties());
@@ -2878,7 +2870,6 @@ public class KafkaBinderTests extends
 
 	@Test
 	@SuppressWarnings("unchecked")
-	@Disabled
 	public void testAutoAddPartitionsDisabledFailsIfTopicUnderPartitionedAndAutoRebalanceDisabled()
 			throws Throwable {
 		KafkaBinderConfigurationProperties configurationProperties = createConfigurationProperties();
@@ -2897,9 +2888,9 @@ public class KafkaBinderTests extends
 		consumerProperties.setInstanceCount(3);
 		consumerProperties.setInstanceIndex(2);
 		consumerProperties.getExtension().setAutoRebalanceEnabled(false);
-//		expectedProvisioningException.expect(ProvisioningException.class);
-//		expectedProvisioningException.expectMessage(
-//				"The number of expected partitions was: 3, but 1 has been found instead");
+		expectedProvisioningException.expect(ProvisioningException.class);
+		expectedProvisioningException.expectMessage(
+				"The number of expected partitions was: 3, but 1 has been found instead");
 		Binding binding = binder.bindConsumer(testTopicName, "test", output,
 				consumerProperties);
 		if (binding != null) {
@@ -3498,8 +3489,7 @@ public class KafkaBinderTests extends
 		}
 	}
 
-	@Test
-	@Disabled
+	@Test(expected = TopicExistsException.class)
 	public void testSameTopicCannotBeProvisionedAgain() throws Throwable {
 		try (AdminClient admin = AdminClient.create(
 				Collections.singletonMap(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG,
@@ -3511,7 +3501,6 @@ public class KafkaBinderTests extends
 				admin.createTopics(Collections
 						.singletonList(new NewTopic("fooUniqueTopic", 1, (short) 1)))
 						.all().get();
-				fail("Expecting TopicExistsException");
 			}
 			catch (Exception ex) {
 				assertThat(ex.getCause() instanceof TopicExistsException).isTrue();
